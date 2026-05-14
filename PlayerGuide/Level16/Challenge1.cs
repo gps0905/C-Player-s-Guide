b9b7b42975a2df18ba399b68ede4cf4c1a9197c2
gps0905 <goncalo.silva0905@gmail.com>
2026-05-14 18:23:00 +0100

    Level 16 Done

---
+++ PlayerGuide/Level16/Challenge1.cs
@@ -0,0 +1,23 @@
+﻿namespace PlayerGuide.Level16;
+
+public class Challenge1
+{
+    public static void Run()
+    {
+        var current = State.Locked;
+
+
+        while (true)
+        {
+            
+            Console.Write($"The chest is {current}. What do you want to do? ");
+            var choice = Console.ReadLine();
+            if (current == State.Locked && choice == "unlock") current = State.Closed;
+            if (current == State.Closed && choice == "lock") current = State.Locked;
+            if (current == State.Closed && choice == "open") current = State.Open;
+            if (current == State.Open && choice == "close") current = State.Closed;
+        }
+    }
+    
+    enum State { Closed, Open, Locked}
+}
